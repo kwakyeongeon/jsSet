53e770f15a93a1a60405236e5633715ee77dcd63
kwakyeongeon <85777526+kwakyeongeon@users.noreply.github.com>
2021-07-24 22:56:42 +0900

    Add files via upload

--- js/coments.js
+++ js/coments.js
@@ -42,7 +42,8 @@ function deleteBtnHandler(event){
     const li = event.target.parentElement;
     const submitPassword = event.target.querySelector("input:nth-child(1)").value;
     coments=coments.filter((coment) => coment.password !== submitPassword);
-    saveComents() //<= 위의 배열에서 선택된 인자의 li를 없애는 기능을 추가해야한다.
+    saveComents()
+    li.remove();
     
 }
 
@@ -83,3 +84,11 @@ if(savedComents){
         
 }
 
+/*
+최종 문제점
+1.댓글 삭제시 비밀번호 가 틀려도 삭제됨 ->그러나 데이터에서 삭제는 되지 않기에 새로고침시 재 생성
+2. 해당 댓글이 아니더라도 같은 비밀번호 입력시 댓글삭제
+
+현재구현 기능
+1.댓글을 데이터에 저장 후 비밀번호 대조 후 삭제 기능
+*/
